4e9e234cffdb1f7c0f77c479d7f394139dc388f2
kidminks <mk9990143266@gmail.com>
2018-12-23 10:08:02 +0530

    drawerscreen basic styling done

--- src/component/DrawerScreen.js
+++ src/component/DrawerScreen.js
@@ -1,37 +1,57 @@
 import React, {Component} from 'react';
 import {NavigationActions} from 'react-navigation';
-import {ScrollView, Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 import { DrawerActions } from 'react-navigation';
 
 class DrawerScreen extends Component {
+    constructor(props){
+        super(props);
+        this.selected_id = 'SetTimer';
+    }
     navigateToScreen = (route) => () => {
         const navigateAction = NavigationActions.navigate({
             routeName: route
         });
         this.props.navigation.dispatch(navigateAction);
-        this.props.navigation.dispatch(DrawerActions.closeDrawer())
+        this.props.navigation.dispatch(DrawerActions.closeDrawer());
+        this.selected_id = route;
     };
 
     render () {
         return (
             <View>
-                <ScrollView>
-                    <View>
-                        <View>
-                            <Text onPress={this.navigateToScreen('SetTimer')}>
-                                Set Timer
-                            </Text>
-                        </View>
-                        <View>
-                            <Text onPress={this.navigateToScreen('MultiExerciseTimer')}>
-                                Multi Exercise
-                            </Text>
-                        </View>
-                    </View>
-                </ScrollView>
+                <View>
+                    <Text
+                        style={
+                            this.selected_id==='SetTimer'?
+                                styles.selected_text:styles.not_selected_text
+                        }
+                        onPress={this.navigateToScreen('SetTimer')}>
+                        Set Timer
+                    </Text>
+                </View>
+                <View>
+                    <Text
+                        style={
+                            this.selected_id==='MultiExerciseTimer'?
+                                styles.selected_text:styles.not_selected_text
+                        }
+                        onPress={this.navigateToScreen('MultiExerciseTimer')}>
+                        Multi Exercise
+                    </Text>
+                </View>
             </View>
         );
     }
 }
-
+const styles = StyleSheet.create({
+    selected_text: {
+        color: 'red',
+        fontSize: 30
+    },
+    not_selected_text: {
+        color: 'yellow',
+        fontSize: 25
+    }
+});
 export default DrawerScreen;
